94786546a80df944876008c8628d9ae44d06ae65
roli <korshun1519811@gmail.com>
2023-05-20 14:33:00 +0300

    Added classes for all additional function, added options and optimized the code 1. Remade a logic of language.py 1.1. Added some new strings 2. More flexible code thanks by new added classes 3. Changed some logical operators for better work of the program 4. Removed some variables for better work of the program 5. Added a new "options" command 5.1. Added an additional command to him 6. Replaced "st.logger" and "st.db" to "logger" and "db"

--- language.py
+++ language.py
@@ -1,129 +1,149 @@
+from telegram import Update, User, ChatMember
+
+
 class Russian:
-    more_args_text = "Слишком много аргументов. Попробуй убрать лишние"
-    idk_command_text = "Неизвестная команда. Попробуй написать команду /help"
-    self1_text = " сам себе"
-    self2_text = " самого себя"
-    no_args_text = "Нет аргументов"
-    no_db_user_text = "Нет в датабазе пользователя"
-    check_cmd_text = "Команд не существовало. Создал новое меню с командами"
-    ok_text = "Все в норме"
-    no_access_text = "У тебя нет доступа к команде, slave"
-    no_cmd_access_text = ("Команда в этом чате недоступна. Попробуйте добавить"
-                          " меня в группу или канал.")
-    no_arg_table_text = "Какую таблицу?"
-    done_text = "Готово, boy"
-    error_args_text = ("Неправильно введена команда. Попробуй проверить данные"
-                       " на ошибки")
-    empty_text = "Пользователь не введён. Попробуй добавить пользователя"
-    start_text = ("Привет, ♂slave♂\n"
-                  "Видимо, ты здесь новенький, давай сам Billy тебя проведёт в"
-                  " ♂deep dark fantasies♂\n"
-                  "Напиши /help для списка команд с которыми ты сможешь "
-                  "использовать силы настоящего ♂full master♂\n"
-                  "И самое главное, ♂slave♂, что сила в мужской дружбе")
-    no_here_text = ("Добавь меня в группу или канал для начала моей грязной "
-                    "работы ;)")
-    cmds_text = (
-        # "/help - Вывести все команды бота",
+    so_many_args = "Слишком много аргументов. Попробуй убрать лишние"
+    idk_cmd_error = "Неизвестная команда. Попробуй написать команду /help"
+    self = " сам себе"
+    self_alt = " самого себя"
+    no_args = "Нет аргументов"
+    no_user_in_db = "Нет в датабазе пользователя"
+    no_cmd_menu = "Команд не существовало. Создал новое меню с командами"
+    ok = "Все в норме"
+    no_access = "У тебя нет доступа к команде, slave"
+    not_here_cmd = ("Команда в этом чате недоступна. Попробуйте добавить меня "
+                    "в группу или канал.")
+    no_table_in_args = "Какую таблицу?"
+    done = "Готово, boy"
+    admin_options_only = "Настройки доступны только для администраторов"
+    not_correct_args_error = ("Неправильно введена команда. Попробуй проверить"
+                              " данные на ошибки")
+    turn_off_cmd = "Данная команда выключена в этом чате"
+    switch = "Включить/Выключить:"
+    no_args_error = "Пользователь не введён. Попробуй добавить пользователя"
+    start = ("Привет, ♂slave♂\n"
+             "Видимо, ты здесь новенький, давай сам Billy тебя проведёт в "
+             "♂deep dark fantasies♂\n"
+             "Напиши /help для списка команд с которыми ты сможешь "
+             "использовать силы настоящего ♂full master♂\n"
+             "И самое главное, ♂slave♂, что сила в мужской дружбе")
+    help = (
+        "/help - Показать это сообщение",
         "/profile - Посмотреть статистику ♂slave♂'а",
         "/lvl - Посмотреть уровень ♂slave♂'а",
         "/cards - Получить случайную картку",
         "/fisting - Сделать ♂fisting♂ ♂slave♂'у",
         "/deep - Проверить глубину ♂slave♂'а",
-        "/warm - Почувствовать тепло ♂slave♂'а"
+        "/warm - Почувствовать тепло ♂slave♂'а",
+        "/options - Настроить бота в группе/канале"
     )
-    for_check_menu_text = (
+    pm_help = (
+        help[0],
+        help[3],
+        "Для полного списка команд, добавь меня в группу или канал"
+    )
+    cmds_menu = (
         "Список команд",
         "Посмотреть статистику ♂slave♂'а",
         "Посмотреть уровень ♂slave♂'а",
         "Получить случайную картку",
         "Сделать ♂fisting♂ ♂slave♂'у",
         "Проверить глубину ♂slave♂'а",
-        "Почувствовать тепло ♂slave♂'а"
+        "Почувствовать тепло ♂slave♂'а",
+        "Настроить бота в группе/канале"
     )
 
-    def __init__(self, user=None, member=None, lvl=None):
-        if user is not None:
-            self.fisting_text = f"{user.first_name} делает жесткий ♂fisting♂"
-            self.deep_text = (f"♂It's so fucking deep♂ - сказал "
-                              f"{user.first_name} boy'ю")
-            self.warm_text = f"{user.first_name} почувствовал тепло"
+    def __init__(self, user: User = None, target: User = None,
+                 lvl: int | float = None):
+        if user:
+            self.fisting = f"{user.first_name} делает жесткий ♂fisting♂"
+            self.deep = (f"♂It's so fucking deep♂ - сказал {user.first_name} "
+                         "boy'ю")
+            self.warm = f"{user.first_name} почувствовал тепло"
             if lvl is not None:
-                self.lvl_text = (f"Welcome to the club, {user.first_name}\n"
-                                 f"Теперь у тебя {lvl} уровень!")
-        if member is not None:
-            if lvl is not None:
-                self.lvl_cmd_text = f"У {member.user.first_name} {lvl} уровень"
-
-    @classmethod
-    def get_cmds_text(cls):
-        output = ""
-        for t in cls.cmds_text:
-            output += t + "\n"
-        return output
+                self.lvl = (f"Welcome to the club, {user.first_name}\n"
+                            f"Теперь у тебя {lvl} уровень!")
+        if target:
+            if lvl:
+                self.lvl_cmd = f"У {target.first_name} {lvl} уровень"
 
 
 class Ukraine:
-    more_args_text = "Занадто багато аргументів. Спробуй видалити зайві"
-    idk_command_text = "Невідома команда. Спробуй написати команду /help"
-    self1_text = " сам собі"
-    self2_text = " самого себе"
-    no_args_text = "Немає аргументів"
-    no_db_user_text = "Немає в датабазі користувача"
-    check_cmd_text = "Команд не існувало. Створив нове меню із командами"
-    ok_text = "Все в нормі"
-    no_access_text = "У тебе немає доступу до команди, slave"
-    no_cmd_access_text = ("Команда в цьому чаті недоступна. Спробуйте додати "
-                          "мене в групу чи канал.")
-    no_arg_table_text = "Яку таблицю?"
-    done_text = "Готово, boy"
-    error_args_text = ("Неправильно введено команду. Спробуй перевірити дані "
-                       "на помилки")
-    empty_text = "Користувач не введений. Спробуй додати користувача"
-    start_text = ("Привіт, ♂slave♂\n"
-                  "Мабуть, ти тут новенький, давай сам Billy тебе проведе в "
-                  "♂deep dark fantasies♂\n"
-                  "Напиши /help для списку команд з якими ти зможеш "
-                  "використовувати сили справжнього ♂full master♂\n"
-                  "І саме головне, ♂slave♂, що сила в чоловічій дружбі")
-    no_here_text = ("Добав мене в групу чи канал для початку моєї брудної "
-                    "праці ;)")
-    cmds_text = (
-        # "/help - Вивести всі команди бота",
+    so_many_args = "Занадто багато аргументів. Спробуй видалити зайві"
+    idk_cmd_error = "Невідома команда. Спробуй написати команду /help"
+    self = " сам собі"
+    self_alt = " самого себе"
+    no_args = "Немає аргументів"
+    no_user_in_db = "Немає в датабазі користувача"
+    no_cmd_menu = "Команд не існувало. Створив нове меню із командами"
+    ok = "Все в нормі"
+    no_access = "У тебе немає доступу до команди, slave"
+    not_here_cmd = ("Команда в цьому чаті недоступна. Спробуйте додати мене в "
+                    "групу чи канал.")
+    no_table_in_args = "Яку таблицю?"
+    done = "Готово, boy"
+    admin_options_only = "Налаштування доступні тільки для адміністраторів"
+    not_correct_args_error = ("Неправильно введено команду. Спробуй перевірити"
+                              " дані на помилки")
+    turn_off_cmd = "Данна команда вимкнена в цьому чаті"
+    switch = "Ввімкнути/Вимкнути:"
+    empty = "Користувач не введений. Спробуй додати користувача"
+    start = ("Привіт, ♂slave♂\n"
+             "Мабуть, ти тут новенький, давай сам Billy тебе проведе в ♂deep "
+             "dark fantasies♂\n"
+             "Напиши /help для списку команд з якими ти зможеш використовувати"
+             " сили справжнього ♂full master♂\n"
+             "І саме головне, ♂slave♂, що сила в чоловічій дружбі")
+    help = (
+        "/help - Показати це повідомлення",
         "/profile - Подивитися статистику ♂slave♂'а",
         "/lvl - Переглянути рівень ♂slave♂'а",
         "/cards - Отримати випадкову картку",
         "/fisting - Зробити ♂fisting♂ ♂slave♂'у",
         "/deep - Перевірити глибину ♂slave♂'а",
-        "/warm - Відчути тепло ♂slave♂'а"
+        "/warm - Відчути тепло ♂slave♂'а",
+        "/options - Налаштувати бота в групі/каналі"
     )
-    for_check_menu_text = (
+    pm_help = (
+        help[0],
+        help[3],
+        "Для повного списку команд, добав мене в групу чи канал"
+    )
+    cmds_menu = (
         "Список команд",
         "Подивитися статистику ♂slave♂'а",
         "Переглянути рівень ♂slave♂'а",
         "Отримати випадкову картку",
         "Зробити ♂fisting♂ ♂slave♂'у",
         "Перевірити глибину ♂slave♂'а",
-        "Відчути тепло ♂slave♂'а"
+        "Відчути тепло ♂slave♂'а",
+        "Налаштувати бота в групі/каналі"
     )
 
-    def __init__(self, user=None, member=None, lvl=None):
-        if user is not None:
-            self.fisting_text = f"{user.first_name} робить жорсткий ♂fisting♂"
-            self.deep_text = (
-                f"♂It's so fucking deep♂ - сказав {user.first_name} "
-                f"boy'ю")
-            self.warm_text = f"{user.first_name} відчув тепло"
-            if lvl is not None:
-                self.lvl_text = (f"Welcome to the club, {user.first_name}\n"
-                                 f"Тепер у тебе {lvl} рівень!")
-        if lvl is not None:
-            if member is not None:
-                self.lvl_cmd_text = f"У {member.user.first_name} {lvl} рiвень"
+    def __init__(self, user: User = None, target: User = None,
+                 lvl: int | float = None):
+        if user:
+            self.fisting = f"{user.first_name} робить жорсткий ♂fisting♂"
+            self.deep = (f"♂It's so fucking deep♂ - сказав {user.first_name} "
+                         f"boy'ю")
+            self.warm = f"{user.first_name} відчув тепло"
+            if not lvl:
+                self.lvl = (f"Welcome to the club, {user.first_name}\n"
+                            f"Тепер у тебе {lvl} рівень!")
+        if target:
+            if lvl:
+                self.lvl_cmd = f"У {target.first_name} {lvl} рiвень"
 
-    @classmethod
-    def get_cmds_text(cls):
-        output = ""
-        for t in cls.cmds_text:
-            output += t + "\n"
-        return output
+
+class Language:
+
+    def __init__(self, update: Update):
+        self.__user = update.effective_user
+
+    def __call__(self, target: User = None, lvl: int | float = None,
+                 *args, **kwargs):
+        if self.__user.language_code == "uk":
+            self.__lang = Ukraine(self.__user, target, lvl)
+        else:
+            self.__lang = Russian(self.__user, target, lvl)
+        return self.__lang
